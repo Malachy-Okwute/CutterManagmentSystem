1fe8c262103f0367448d1f89a99d5567f99f7847
Malachy Okwute <47065240+Mal-kingston@users.noreply.github.com>
2024-11-17 20:08:35 -0500

    update

--- CMS/ViewModels/CMSWindowViewModel.cs
+++ CMS/ViewModels/CMSWindowViewModel.cs
@@ -35,6 +35,9 @@ namespace CMS
         /// </summary>
         private double _resizeBorderSize = 8;
 
+        /// <summary>
+        /// The main application window
+        /// </summary>
         private Window _appWindow => Application.Current.MainWindow;
 
         #endregion
@@ -120,89 +123,11 @@ namespace CMS
         /// </summary>
         public CMSWindowViewModel()
         {
-            //// Screen work area
-            //Rect workArea = SystemParameters.WorkArea;
-
-            //// Hook into window size changed event
-            //_appWindow.SizeChanged += (s, e) =>
-            //{
-            //    // Set is-maximized to true if window is maximized
-            //    IsMaximized = _appWindow.WindowState == WindowState.Maximized;
-
-            //    // Set resize border with to 0 when window is maximized and 8 if window is not maximized
-            //    //ResizeBorderSize = _appWindow.WindowState == WindowState.Maximized ? 0 : 8;
-
-            //    #region Snap Layout
-
-            //    // The new size layout to snap this window to
-            //    Size newSize = e.NewSize;
-
-            //    // Snap to left or right equal half of the snap layout
-            //    if (newSize.Width == workArea.Width / 2 && newSize.Height == workArea.Height)
-            //        DropShadowPadding = 0;
-            //    // Snap to either of the 4 equal sections of the snap layout corners
-            //    else if (newSize.Width == workArea.Width / 2 && newSize.Height == workArea.Height / 2)
-            //        DropShadowPadding = 0;
-            //    // Snap to either of the 3 equal sections of the snap layout
-            //    else if (newSize.Width == workArea.Width / 3 && newSize.Height == workArea.Height)
-            //        DropShadowPadding = 0;
-
-            //    // Snap to center of the 3 unequal sections of the snap layout
-            //    else if (newSize.Width == GetSnapLayoutSectionSizeUsingPercentageValue(workArea.Width, 43.958) && newSize.Height == workArea.Height)
-            //        DropShadowPadding = 0;
-            //    // Snap to either sides of the 3 unequal sections of the snap layout
-            //    else if (newSize.Width == GetSnapLayoutSectionSizeUsingPercentageValue(workArea.Width, 28.021) && newSize.Height == workArea.Height)
-            //        DropShadowPadding = 0;
-            //    // Snap to the 2/3 of section of the snap layout
-            //    else if (newSize.Width == (workArea.Width / 3) * 2 && newSize.Height == workArea.Height)
-            //        DropShadowPadding = 0;
-            //    // Otherwise
-            //    else
-            //    {
-            //        DropShadowPadding = 40;
-            //        ResizeBorderSize = 8;
-            //    }
-
-            //    #endregion
-            //};
-
             // Create commands
-            MinimizeWindowCommand = new RelayCommand(MinimizeWindow, canExecuteCommand => this != null);
+            MinimizeWindowCommand = new RelayCommand(() => _appWindow.WindowState = WindowState.Minimized, canExecuteCommand => this != null);
             //MaximizeWindowCommand = new RelayCommand(() => _appWindow.WindowState ^= WindowState.Maximized, canExecuteCommand => this != null);
-            CloseWindowCommand = new RelayCommand(CloseWindow, canExecuteCommand => this != null);
+            CloseWindowCommand = new RelayCommand(() => _appWindow.Close(), canExecuteCommand => this != null);
         }
-
-        #endregion
-
-        #region Command Methods
-
-        /// <summary>
-        /// Minimizes <see cref="CMSWindow"/>
-        /// </summary>
-        private void MinimizeWindow()
-        {
-            _appWindow.WindowState = WindowState.Minimized;
-        }
-
-        /// <summary>
-        /// Closes <see cref="CMSWindow"/>
-        /// </summary>
-        private void CloseWindow()
-        {
-            _appWindow.Close();
-        }
-
-        #endregion        
-        
-        #region Private Methods
-
-        /// <summary>
-        /// Gets the actual width of a section of window layout to snap to using the supplied percentage value
-        /// </summary>
-        /// <param name="maxWorkAreaSize">The max work area size</param>
-        /// <param name="percentageValue">The percentage value to use</param>
-        /// <returns>Number value</returns>
-        private double GetSnapLayoutSectionSizeUsingPercentageValue(double maxWorkAreaSize, double percentageValue) => Math.Round((percentageValue / 100) * maxWorkAreaSize);
 
         #endregion
     }
